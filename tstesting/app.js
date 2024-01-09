7c38ffe8bb5b398bb3ebe9fa86bb362581bb41b3
Daniel Umejiego <danielumejiego@yahoo.com>
2024-01-08 17:25:16 -0800

    Initial commit

---
+++ tstesting/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(sum(5, 3));
+console.log(sum(5, 4));
---
+++ tstesting/app.ts
@@ -0,0 +1,6 @@
+const sum = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+console.log(sum(5, 3));
+console.log(sum(5, 4));
