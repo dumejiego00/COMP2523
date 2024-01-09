8deb9c9944b3509fe1dafbdbbd76431f58da4e92
Daniel Umejiego <danielumejiego@yahoo.com>
2024-01-08 17:57:29 -0800

    Eighth commit

--- Class1/app.js
+++ Class1/app.js
@@ -3,4 +3,4 @@ const twoPointCartesianDistance = (x1, y1, x2, y2) => {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 };
 console.log(twoPointCartesianDistance(3, 2, 9, 7));
-console.log(Math.sqrt(61));
+console.log(Math.sqrt(61)); //Correct answer
