d01cf493ffed3912cc861e6c3c76c57023f047ee
Daniel Umejiego <danielumejiego@yahoo.com>
2024-01-08 17:51:36 -0800

    Second commit

--- Class1/app.ts
+++ Class1/app.ts
@@ -3,9 +3,9 @@ const twoPointCartesianDistance = (
   y1: number,
   x2: number,
   y2: number
-): number => {
+):number => {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 };
 
 console.log(twoPointCartesianDistance(3, 2, 9, 7));
-console.log(Math.sqrt(61))
+console.log(Math.sqrt(61));
